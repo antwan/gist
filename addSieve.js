94bc720209642f37f7417cea052308908df003d3
Antoine <3027728+antwan@users.noreply.github.com>
2019-10-04 11:18:51 +0100

    Update addSieve.js

--- addSieve.js
+++ addSieve.js
@@ -1,8 +1,10 @@
 function addSieve(){
   $('h1.mb-1').html('\
     <span>Gmail</span>\
-    <input type="checkbox" class="custom-control-input" id="sieve-switcher">\
-    <label class="custom-control-label" for="sieve-switcher"></label>\
+    <span class="custom-switch">\
+      <input type="checkbox" class="custom-control-input" id="sieve-switcher">\
+      <label class="custom-control-label" for="sieve-switcher"></label>\
+    </span>\
     <span>Sieve</span>\
   ');
 }
